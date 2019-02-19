118936b01e08850bcb2b35b362531e00b906ba3c
Mohan Madhavan <mohan.madhavan@yahoo.com>
2019-02-18 23:41:21 -0500

    modified scaffold file

--- lib/scaffoldExample.dart
+++ lib/scaffoldExample.dart
@@ -1,33 +1,54 @@
 import "package:flutter/material.dart";
 
-
-class ScaffoldHome extends StatelessWidget{
-
+class ScaffoldHome extends StatelessWidget {
   String title;
 
-  ScaffoldHome({Key key, this.title}): super(key: key);
+  ScaffoldHome({Key key, this.title}) : super(key: key);
 
-  Scaffold createScafflod(){
+  Scaffold createScafflod() {
     /*
       Scaffold has many in-built widgets like AppBar,
       floatingActionButton, bottomNavigationBar, body etc..
 
-      So, we dclare the properties of these widgets and return
+      So, we declare the properties of these widgets and return
       the scaffold
 
      */
 
     return new Scaffold(
-
       // Creates and sets params for AppBar
       appBar: new AppBar(
-        backgroundColor: Colors.deepOrange.shade700,
-        title: new Text(this.title) ,
-      ),
+          backgroundColor: Colors.deepOrange.shade700,
+          title: new Text(this.title),
+          // The action widgets are the right icons in the appBar
+          actions: <Widget>[
+            new IconButton(
+                icon: new Icon(Icons.send),
+                onPressed: () => debugPrint("Send icon Tapped!")),
+            new IconButton(
+                icon: new Icon(Icons.search),
+                onPressed: () => debugPrint("Send icon Tapped!")),
+          ]),
 
       // Creates and sets params for body of scaffold
+      // The hierarchy of the body is:
+      // Container -> Column -> [Inkwell]
       body: new Container(
         color: Colors.grey,
+        // set the alignment of the container to center
+        alignment: Alignment.center,
+        child: new Column(
+            // align the column also the center
+            mainAxisAlignment: MainAxisAlignment.center,
+            // The column can have many children each of which will
+            // be a row in the column
+            children: <Widget>[
+              // Inkwell is a rectangular area of a Material that responds to touch.
+              new InkWell(
+                child: new Text("Button!"),
+                onTap: () => debugPrint("Button Tapped!"),
+              ),
+            ]),
       ),
 
       // Creates and sets params for floatingActionButton
@@ -42,24 +63,29 @@ class ScaffoldHome extends StatelessWidget{
       // Creates and sets params for bottomNavigationBar
       // Note: bottomNavigationBar requires atleast 2 items
       bottomNavigationBar: new BottomNavigationBar(
-
         // have to declare the type if there are more
         // than 3 navigation items
         type: BottomNavigationBarType.fixed,
 
         // The actual buttons
         items: [
-
-          new BottomNavigationBarItem(icon: new Icon(Icons.account_box, color: Colors.black26), title: new Text("Account")),
-          new BottomNavigationBarItem(icon: new Icon(Icons.restaurant_menu, color: Colors.black26), title: new Text("Menu")),
-          new BottomNavigationBarItem(icon: new Icon(Icons.pages, color: Colors.black26), title: new Text("Options")),
-          new BottomNavigationBarItem(icon: new Icon(Icons.settings, color: Colors.black26), title: new Text("Settings"))
+          new BottomNavigationBarItem(
+              icon: new Icon(Icons.account_box, color: Colors.black26),
+              title: new Text("Account")),
+          new BottomNavigationBarItem(
+              icon: new Icon(Icons.restaurant_menu, color: Colors.black26),
+              title: new Text("Menu")),
+          new BottomNavigationBarItem(
+              icon: new Icon(Icons.pages, color: Colors.black26),
+              title: new Text("Options")),
+          new BottomNavigationBarItem(
+              icon: new Icon(Icons.settings, color: Colors.black26),
+              title: new Text("Settings"))
         ],
 
         // action handler for item pressed (note: its index based)
         onTap: (int i) => debugPrint("Hey Touched $i"),
       ),
-
     );
   }
 
@@ -67,9 +93,6 @@ class ScaffoldHome extends StatelessWidget{
   // Note: This function builds and returns the StatelessWidget
   @override
   Widget build(BuildContext context) {
-
     return createScafflod();
-
   }
-
 }
