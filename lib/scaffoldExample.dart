2370cec654fa365d7dc2265ad0dab419655ef67a
Mohan Madhavan <mohan.madhavan@yahoo.com>
2019-02-20 13:46:24 -0500

    Building custom widgets, Interacting with scaffold to show snackbar, Fetching the default values from Theme attributes and creating GestureDetectors

--- lib/scaffoldExample.dart
+++ lib/scaffoldExample.dart
@@ -34,7 +34,7 @@ class ScaffoldHome extends StatelessWidget {
       // The hierarchy of the body is:
       // Container -> Column -> [Inkwell]
       body: new Container(
-        color: Colors.grey,
+        //color: Colors.grey,
         // set the alignment of the container to center
         alignment: Alignment.center,
         child: new Column(
@@ -44,10 +44,15 @@ class ScaffoldHome extends StatelessWidget {
             // be a row in the column
             children: <Widget>[
               // Inkwell is a rectangular area of a Material that responds to touch.
+              // it has the onTap function keyword that responds to touchs
               new InkWell(
-                child: new Text("Button!"),
+                child: new Text("InkWell Button!"),
                 onTap: () => debugPrint("Button Tapped!"),
               ),
+
+              // Adding a custom button (see below for details on how
+              // this button is built)
+              new CustomButton(),
             ]),
       ),
 
@@ -94,5 +99,60 @@ class ScaffoldHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return createScafflod();
+  }
+}
+
+// Build a custom widget (here a custom button which responds to
+// gestures ) and return it to one of the children in the
+// container column of the body
+// Note: This is a separate class and not part of the Scaffold class
+//       above
+class CustomButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      // Assign a event handler to opTap that responds to tap
+      // Note: onTap should be a function returning a widget
+      // onTap: createSnackBar(context) will not work
+      onTap: () => createSnackBar(context),
+
+      // Create a container that will serve as a button and
+      // since this container is a child of GestureDetector
+      // it will respond to all the touch events of the
+      // GestureDetector
+      child: new Container(
+        // Create a text field that serves as the button text
+        child: new Text("Custom Button"),
+
+        // Note: Since the container is a large canvas we need
+        //       decorate it to look like a button
+        decoration: new BoxDecoration(
+            // Note: We get the default button color based on the current
+            //       material theme by calling the Theme attribute
+            color: Theme.of(context).buttonColor,
+
+            // assigning a border radius to the container
+            borderRadius: new BorderRadius.circular(5.0)),
+
+        // We need this padding because the container wraps around
+        // the largest child inside it so without this padding the container
+        // would just be the size of the Text field
+        padding: new EdgeInsets.all(18.0),
+      ),
+    );
+  }
+
+  // This function serves as an event handler for the GestureDetector's onTap
+  createSnackBar(BuildContext context) {
+    // create a snackbar
+    final sb = new SnackBar(content: new Text("Button tapped"));
+
+    // attach a snack bar to the scaffold
+
+    // This line translates to, show snack bar using the
+    // scaffold who's context is 'context'
+    // Note: Scaffold (like the Theme var) is a global attribute
+    //       which we can make use of in such scenarios
+    Scaffold.of(context).showSnackBar(sb);
   }
 }
